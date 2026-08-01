604ffe1745a1d949b85e0dec5ec86ce161c3974b
zandinaghi-alt <zandinaghi@gmail.com>
2026-08-01 10:37:48 +0330

    Initial commit

---
+++ app/src/main/java/com/mushroomfarm/accounting/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.mushroomfarm.accounting
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true   // needed so localStorage keeps the worker/factory data
+        settings.databaseEnabled = true
+        settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        settings.allowFileAccess = true
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
